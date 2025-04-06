571e27a2356505f93891310451b5644bfd618629
florentak <florentkossiayidedji@gmail.com>
2025-04-06 22:16:47 +0100

    fix: revue de la route de l'admin pour récupérer les infos pour le dashboard

--- backend/index.js
+++ backend/index.js
@@ -3,29 +3,36 @@ const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/database');
 
+// Import des routes
 const authRoutes = require('./routes/auth.routes');
-const adminRouter = require("./routes/admin.routes");
-const userRouter = require("./routes/user.routes");
+const adminRoutes = require("./routes/admin.routes");
+const userRoutes = require("./routes/user.routes");
+const bookingRoutes = require("./routes/booking.routes");
+const roomRoutes = require("./routes/room.routes");
+const hotelRoutes = require("./routes/hotel.routes");
+const roomTypeRoutes = require("./routes/roomType.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/', authRoutes);
-app.use('/', adminRouter);
-app.use('/', userRouter);
-
+app.use('/admin', adminRoutes);
+app.use('/users', userRoutes);
+app.use('/bookings', bookingRoutes);
+app.use('/rooms', roomRoutes);
+app.use('/hotels', hotelRoutes);
+app.use('/room-types', roomTypeRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-/* Teste de la connexion à la database et demarrage du server */
+/* Teste de la connexion à la database et démarrage du serveur */
 sequelize.authenticate()
   .then(() => {
     console.log('Connexion à la base de données réussie.');
     app.listen(PORT, () => {
-      console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
+      console.log(`Serveur démarré sur http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
