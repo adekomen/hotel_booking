61fee49fcd8de8964afdfcbca579c9ad35b0373e
adekomen <k.francoisadesu@gmail.com>
2025-04-28 14:06:40 +0100

    Fix: Update database connection config for deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
 const config = require('./config.js');
 
-const sequelize = new Sequelize(config.development.database, config.development.username, config.development.password, {
-  host: config.development.host,
-  dialect: config.development.dialect,
-  port: config.development.port,
-});
+const sequelize = new Sequelize(
+  config.development.database,
+  config.development.username,
+  config.development.password,
+  {
+    host: config.development.host,
+    dialect: config.development.dialect,
+    port: config.development.port,
+    logging: console.log,
+  }
+);
 
 module.exports = sequelize;
